ebe8b0f30c6f24485caec3587ca465a56ecd1f3e
Nguyễn Văn Quyết <chiengaphuly299@gmail.com>
2024-02-27 21:20:20 +0700

    Viết API lấy tất cả thông tin người dùng

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -21,6 +21,26 @@ let handleLogin = async (req, res) => {
   });
 };
 
+let handleGetAllUsers = async (req, res) => {
+  let id = req.body.id;
+
+  if (!id) {
+    return res.status(500).json({
+      errCode: 1,
+      message: "Missing input parameter!",
+    });
+  }
+
+  let users = await userService.getAllUsers(id);
+
+  return res.status(200).json({
+    errCode: 0,
+    errMessage: "OK",
+    users,
+  });
+};
+
 module.exports = {
   handleLogin: handleLogin,
+  handleGetAllUsers: handleGetAllUsers,
 };
